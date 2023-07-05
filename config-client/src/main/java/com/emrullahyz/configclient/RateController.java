f6f6526e3f00a86b199e54f91055b09f1481c06b
Emrullah Yilmaz <emrullahyilmzv2@gmail.com>
2023-07-05 18:38:17 +0300

    encrypt and decrypt configs demo

--- config-client/src/main/java/com/emrullahyz/configclient/RateController.java
+++ config-client/src/main/java/com/emrullahyz/configclient/RateController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class RateController {
@@ -14,11 +15,15 @@ public class RateController {
     @Value("${tollstart}")
     String tollstart;
 
-    @GetMapping("/rate")
+    @Value("${connstring}")
+    String connstring;
+
+    @RequestMapping("/rate")
     public String getRate(Model m){
         m.addAttribute("rateamount",rate);
         m.addAttribute("lanes", lanecount);
         m.addAttribute("tollstart", tollstart);
+        m.addAttribute("connstring", connstring);
         return "rateview";
     }
 }
