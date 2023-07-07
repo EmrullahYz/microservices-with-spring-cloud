0daff0d34b8c411e2e532bbcf6b661526727ccc6
Emrullah Yilmaz <emrullahyilmzv2@gmail.com>
2023-07-07 22:36:02 +0300

    refresh added to client project

--- config-client/src/main/java/com/emrullahyz/configclient/RateController.java
+++ config-client/src/main/java/com/emrullahyz/configclient/RateController.java
@@ -1,11 +1,13 @@
 package com.emrullahyz.configclient;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@RefreshScope
 @Controller
 public class RateController {
     @Value("${rate}")
